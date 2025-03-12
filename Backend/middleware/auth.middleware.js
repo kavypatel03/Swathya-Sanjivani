aae954db8b90537076a5c8c56e3ff237bd26c5c4
kavypatel03 <kavyrajeshbhaivanani@gmail.com>
2025-03-12 22:32:19 +0530

    Frontend and backend test

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const patientModel = require('../models/patient.model');
+
+module.exports = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized - No token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await patientModel.findById(decoded._id).select('-password');
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid Token or Expired Session' });
+    }
+};
---
+++ Backend/models/patient.model.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const patientSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength:[10,"Mobile Number Must Be Exactly 10 characters"],
+        maxlength:[10,"Mobile Number Must Be Exactly 10 characters"]
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        minlength:[14,"Please Enter a Valid Email Address"]
+    },
+    password : {
+        type: String,
+        required: true,
+        select : false,
+    },
+    dob : {
+        type: Date,
+    },
+    age : {
+        type: Number,
+    },
+    relation : {
+        type: String,
+    },
+    gender : {
+        type: String,
+    },
+    familyMember : {
+        type: String,
+    },
+    documents : {
+        type: Array,
+        default: []
+    }
+});
+
+patientSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+patientSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this._password);
+}
+
+patientSchema.statics.hashPassword = async function (password){
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+const patientModel = mongoose.model('patient', patientSchema);
+
+module.exports = patientModel;
