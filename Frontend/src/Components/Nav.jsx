207225625be18f68d3663434635e60bfbe0546eb
kavypatel03 <kavyrajeshbhaivanani@gmail.com>
2025-03-18 01:36:02 +0530

    Final Test

--- Frontend/src/Components/Nav.jsx
+++ Frontend/src/Components/Nav.jsx
@@ -16,7 +16,7 @@ const Navbar = () => (
             color="currentColor"
             className="mr-1"
             />
-            Register Now</Link>
+            Register</Link>
     </nav>
 );
 
